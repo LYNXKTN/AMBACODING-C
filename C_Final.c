01f6afca6fd972cacff9491cb0190969fda28456
LYNXKTN <155950712+LYNXKTN@users.noreply.github.com>
2026-06-01 05:33:09 +0700

    Update team name description in C_Final.c

---
+++ C_Final.c
@@ -0,0 +1,49 @@
+/* ===================================================================
+SAS INFORMATIKA
+-----------------------------------------------------------------
+Nama Tim    : kata arkan namanya AMBACODING 100% Halal jadi kita iyain ajah
+Anggota     : 1. Azaria Arkana Mulya, 2. Muhammad Wildan Muzakki, 3. Ahmad Nejat Rahmansyah
+Kelas       : X-A/B
+Pelajaran   : Informatika (SAS)
+=================================================================== */
+
+#include <stdio.h>
+
+int main() {
+    int jumlah_log;          
+    float suhu, total = 0;   
+    float rata_rata;
+    int i = 1;               
+
+    printf("=== SISTEM MONITORING SUHU HARDWARE===\n");
+    printf("Masukkan berapa kali pengecekan suhu: ");
+    scanf("%d", &jumlah_log); 
+
+    printf("\n-----------------------------------\n");
+
+        while (i <= jumlah_log) {
+        printf("Masukkan data suhu ke-%d (°C): ", i);
+        scanf("%f", &suhu); 
+
+        total = total + suhu; 
+
+        if (suhu >= 85.0) {
+            printf("   [PERINGATAN] Suhu ekstrem (%s)! Bahaya Overheat.\n", ">= 85 C"); 
+        } else if (suhu >= 40.0 && suhu < 85.0) {
+            printf("   [INFO] Suhu dalam batas normal (40 C - 84.9 C).\n");
+        } else {
+            printf("   [PERINGATAN] Suhu terlalu rendah (< 40 C)! Kurang optimal.\n");
+        }
+
+        i++; 
+    }
+
+    rata_rata = total / jumlah_log;
+
+    printf("-----------------------------------\n");
+    printf("=== LAPORAN PERFORMA SYSTEM ===\n");
+    printf("Total Pengecekan : %d kali\n", jumlah_log);
+    printf("Rata-rata Suhu   : %.2f °C\n", rata_rata); 
+
+    return 0; 
+}
